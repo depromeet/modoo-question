93aef75e7f90efde9d8a9b6057ea910b190d36b4
JiwoonKim <diana0501@naver.com>
2019-11-11 19:27:29 +0900

    QuestionList 컨테이너 추가

---
+++ src/containers/QuestionList/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from '@emotion/styled'
+import Question from '../../components/Question'
+
+function QuestionList( {list} ) {
+
+  return (
+    <Wrapper>
+        {list.map(q => (
+            <Question questionText={q.text} likeNum={q.likeNum} liked={q.liked} />
+        ))}
+    </Wrapper>
+    
+  );
+}
+
+export default React.memo(QuestionList);
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  & > *:not(:last-child) {
+    margin-bottom: 16px;
+  }
+`;
