10ef22886fd5c81fb2eaa1448c6ec67d529628c9
JiwoonKim <diana0501@naver.com>
2019-11-18 20:48:36 +0900

    라우팅 추가

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import Main from './containers/Main'
+import { Switch, Route } from 'react-router-dom';
+import Enter from './containers/Enter';
+import Main from './containers/Main';
 
 export default function App() {
-    return (
-        <Main />
-    );
+  return (
+    <Switch>
+      <Route exact path='/' component={Enter} />
+      <Route exact path='/:roomId' component={Main} />
+    </Switch>
+  );
 };
