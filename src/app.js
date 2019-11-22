7daca31fad8b42c348a7053764de272c7c6b5b34
JiwoonKim <diana0501@naver.com>
2019-11-22 18:45:28 +0900

    app 스타일 추가

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Enter from './pages/Enter';
 import Main from './pages/Main';
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={Enter} />
-      <Route exact path='/:roomId' component={Main} />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Enter} />
+        <Route exact path='/:roomId' component={Main} />
+      </Switch>
+    </BrowserRouter>
   );
 };
