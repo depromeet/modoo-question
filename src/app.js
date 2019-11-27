6b694115f5ed171f2366ae2fd3c6655c4c4d21ec
JiwoonKim <diana0501@naver.com>
2019-11-27 17:42:25 +0900

    세미나 방 및 유저 상태 추가

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Enter from './pages/Enter';
 import Main from './pages/Main';
 
 export default function App() {
+  
+  const [userId, setUserId] = useState(1);
+  const [seminarRoom, setSeminarRoom] = useState({seminarId: 1234, seminarTitle: '디프만 외부 세미나'});
+
   return (
     <BrowserRouter>
       <Switch>
