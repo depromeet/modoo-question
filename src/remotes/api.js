d63687bc2d8534166eab172e70b5c051839b3a93
JiwoonKim <diana0501@naver.com>
2019-11-28 19:09:33 +0900

    Enter 페이지 새 세미나 방 입장 API 연동

--- src/remotes/api.js
+++ src/remotes/api.js
@@ -2,16 +2,24 @@ import * as axios from 'axios';
 
 export const API_ROOT = 'http://13.125.252.156:8081';
 
-export async function createSeminarRoom(fullUrl, shortUrl, title, password) {
+export async function createSeminarRoom(fullUrl, title, password) {
   const { data } = await axios.post(`${API_ROOT}/api/seminar`, {
-    seminarRoom: {
-      "fullURL": fullUrl,
-      "seminarPassword": password,
-      "seminarTitle": title,
-      "shortURL": shortUrl,
-    },
+    "fullURL": fullUrl,
+    "seminarPassword": password,
+    "seminarTitle": title
   });
 
   return data;
 }
   
+export async function createMember(seminarId) {
+  const { data } = await axios.post(`${API_ROOT}/api/member/join`, {
+    "seminarId": seminarId
+  });
+  return data;
+}
+
+export async function enterSeminar(seminarId, userId) {
+  const { data } = await axios.get(`${API_ROOT}/api/seminar/enter/${seminarId}/${memberId}`);
+  return data;
+}
