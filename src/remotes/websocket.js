ef416816c4760dd1c15f713cfd01bfd86e0c308b
JiwoonKim <diana0501@naver.com>
2019-12-07 20:06:06 +0900

    Main 페이지 minor 업데이트

--- src/remotes/websocket.js
+++ src/remotes/websocket.js
@@ -8,13 +8,8 @@ import { QuestionContext, RankingContext } from '../contexts';
 export const STOMP_ENDPOINT = '/q-rank-websock';
 export const DESTINATION_PREFIX = '/app';
 export const SIMPLE_BROKER = '/subscribe';
-export let stompClient = null;
 
-// Contexts
-export function Context() {
-  const { setQuestionList } = useContext(QuestionContext);
-  //const { setRankingList } = useContext(RankingContext);
-}
+export let stompClient = null;
 
 export function connectWebSockets(seminarId) {
   const socket = new SockJS(`${API_ROOT}/${STOMP_ENDPOINT}`);
@@ -35,26 +30,25 @@ export function connectWebSockets(seminarId) {
   );
 };
 
+/*
 export function receiveBroadcasting(data) {
-  console.log(data);
+  const { addNewQuestion, updateLikeCount, deleteQuestion } = useContext(QuestionContext);
+  const { setRankings } = useContext(RankingContext);
 
-  /*
-  // TODO: Question, Ranking Context 업데이트
-  //       data에서 comment, ranking 따로 파싱 필요
   if (data.type === 'comment') {
-    //setQuestionList(prev => {
-      // TODO: 이전의 LIST에 새 COMMENT 붙이기
-    })
-  } else if (data.type === 'like' || data.type === 'unlike') {
-    //setQuestionList({ })
-      // prev.reduce
-      // TODO: commentId 매칭하는 comment의 likeCount 변경
-      //       questionList 전체를 다시 반환해야 함
-  } else if (data.type === 'ranking') {
-    //setRankingList(ranking);
+    addNewQuestion(speaker, question);
   }
-  */
+  if (data.type === 'like' || data.type === 'unlike') {
+    updateLikeCount(speaker, question, likeCount);
+  }
+  if (data.type === 'ranking') {
+    setRankings(speaker, rankings);
+  }
+  if (data.type === 'delete') {
+    deleteQuestions(speaker, question);
+  }
 };
+*/
 
 export function postQuestion(seminarId, question) {
   stompClient.send(`${DESTINATION_PREFIX}/comment/${seminarId}`, {}, question);
