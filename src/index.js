f492fefe8d0f7c366ac79c0eb2531f13ae5981c5
yongseongjeon <yongseong.jeon95@gmail.com>
2019-12-13 13:55:27 +0900

    Pull origin/master

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> refs/remotes/origin/master
--- src/remotes/api.js
+++ src/remotes/api.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import * as axios from 'axios';
-
-export const API_ROOT = 'http://13.125.252.156:8081';
-
-export async function createSeminarRoom(fullUrl, title, password) {
-  const { data } = await axios.post(`${API_ROOT}/api/seminar`, {
-    "fullURL": fullUrl,
-    "seminarPassword": password,
-    "seminarTitle": title
-  });
-
-  return data;
-}
-  
-export async function createMember(seminarId) {
-  const { data } = await axios.post(`${API_ROOT}/api/member/join`, {
-    "seminarId": seminarId
-  });
-  return data;
-}
-
-export async function enterSeminar(seminarId, userId) {
-  const { data } = await axios.get(`${API_ROOT}/api/seminar/enter/${seminarId}/${userId}`);
-  return data;
-}
-=======
 import * as axios from 'axios';
 
 export const API_ROOT = 'http://13.125.252.156:8081';
@@ -62,4 +35,3 @@ export async function enterSeminarAdmin(seminarId, userId) {
   const { data } = await axios.get(`${API_ROOT}/api/seminar/enter/${seminarId}/${userId}`);
   return data;
 }
->>>>>>> refs/remotes/origin/master
